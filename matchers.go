a36228432b98f25c5f64ac45e6d757225fc29aac
mar.maluszczak@gmail.com <297255+malumar@users.noreply.github.com>
2025-11-30 13:41:53 +0100

    IsLower... functions

---
+++ matchers.go
@@ -0,0 +1,34 @@
+package strutils
+
+func IsLowerAlphaNum(s string) bool {
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if (c < 'a' || c > 'z') && (c < '0' || c > '9') {
+			return false
+		}
+	}
+	return true
+}
+
+func IsLowerAlphaNumDashUnderscore(s string) bool {
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if (c < 'a' || c > 'z') && (c < '0' || c > '9') && c != '_' && c != '-' {
+			return false
+		}
+		if i == 0 || i == len(s)-1 && (c == '_' || c == '-') {
+			return false
+		}
+	}
+	return true
+}
+
+func IsLowerASCII(s string) bool {
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c < 'a' || c > 'z' {
+			return false
+		}
+	}
+	return len(s) > 0
+}
